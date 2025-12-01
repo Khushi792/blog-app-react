6c54a0f98549b51a5281bbf5ccb1ce8d4c0b53fc
KHUSHI AGRAWAL <118662644+Khushi792@users.noreply.github.com>
2025-12-01 09:33:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import BlogDetails from "./pages/BlogDetails";
 import Users from "./pages/Users";
 import UserDetails from "./pages/UserDetails";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        {/* Default Home route */}
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<BlogDetails />} />
         <Route path="/users" element={<Users />} />
         <Route path="/users/:id" element={<UserDetails />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
